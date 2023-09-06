720f2b2ad6a880c6e7d9ffee7ea10b15231c4ef3
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2023-09-06 18:01:44 +0530

    navbar refacto + responsive design

--- Component/Navbar/Navbar.js
+++ Component/Navbar/Navbar.js
@@ -1,38 +1,57 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import classes from "./Navbar.module.css";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { FaHamburger } from "react-icons/fa";
 
 const Navbar = (props) => {
   const router = useRouter();
   const path = router.pathname;
+  const [open, setOpen] = useState(false);
+  const onClickListener = () => {
+    setOpen((prevState) => !prevState);
+  };
   return (
     <React.Fragment>
       <header className={`${classes.container}`}>
         <span className={`${classes.logo}`}>Bonappetit</span>
-        <nav className={`${classes.nav_container}`}>
-          <Link
-            href="/"
+        <nav>
+          <FaHamburger
+            className={`${classes.menu_icon}`}
+            onClick={onClickListener}
+          />
+          <div
             className={
-              path === "/"
-                ? `${classes.link} ${classes.link_active}`
-                : `${classes.link}`
+              open
+                ? `${classes.nav_container} ${classes.nav_container_dropdown}`
+                : `${classes.nav_container}`
             }
           >
-            home
-          </Link>
-          <Link
-            href="/meal/bookmark"
-            className={
-              path === "/meal/bookmark"
-                ? `${classes.link} ${classes.link_active}`
-                : `${classes.link}`
-            }
-          >
-            bookmark
-          </Link>
+            <Link
+              href="/"
+              className={
+                path === "/"
+                  ? `${classes.link} ${classes.link_active}`
+                  : `${classes.link}`
+              }
+              onClick={onClickListener}
+            >
+              home
+            </Link>
+            <Link
+              href="/meal/bookmark"
+              className={
+                path === "/meal/bookmark"
+                  ? `${classes.link} ${classes.link_active}`
+                  : `${classes.link}`
+              }
+              onClick={onClickListener}
+            >
+              bookmark
+            </Link>
+          </div>
         </nav>
       </header>
     </React.Fragment>
--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,5 @@ import React from "react";
 import MainPageComponent from "@/Component/MainPageComponent";
 
 export default function Home() {
-  return (
-    <React.Fragment>
-      <MainPageComponent />
-    </React.Fragment>
-  );
+  return <React.Fragment>{/* <MainPageComponent /> */}</React.Fragment>;
 }
