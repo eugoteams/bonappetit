b441d5fa0e3aed48662c370108d1421e1e00a0c4
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2023-09-06 18:33:46 +0530

    adding meta tags

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charset="UTF-8" />
+        <meta
+          name="description"
+          content="World recipes that can be cooked in 30 minutes"
+        />
+        <meta name="keywords" content="food, food recipes ,curry ,indian" />
+        <meta name="author" content="maturi karthik" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
         {/* Google Fonts Roboto */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
