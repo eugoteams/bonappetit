db792d32bee83c1494ddbfb877297db5bfe264c7
maturi karthik <maturikarthik@maturis-MacBook-Pro.local>
2023-09-06 22:21:33 +0530

    removing padding of radio

--- pages/_app.js
+++ pages/_app.js
@@ -10,11 +10,25 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>Recipe's around the world</title>
+        <meta charset="UTF-8" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        ></meta>
+        <meta name="title" content="bonappetit" />
         <meta
           name="description"
-          content="which gets all the meals available for free"
+          content="Discover the art of culinary delight with our mouthwatering food recipe collection. From savory classics to delectable desserts, our easy-to-follow recipes will satisfy your taste buds and elevate your cooking skills. Explore a world of flavors and cooking tips, all at your fingertips. Join our culinary community today and embark on a delicious journey of gastronomic excellence."
         />
-        <meta name="authot" content="Foody Pvt. Ltd." />
+        <meta
+          name="keywords"
+          content="Food recipes,Culinary inspiration,Quick and easy recipes,Healthy meal ideas,Gourmet dishes,Vegetarian  recipes, vegan recipes,Baking and desserts,International cuisines,Chef-approved recipes,Food blog,Cooking for beginners"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="1 days" />
+        <meta name="author" content="maturi karthik" />
       </Head>
       <Navbar />
       <Layout>
